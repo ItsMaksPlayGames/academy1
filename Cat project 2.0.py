42536f4f2f0d3a8a4ae8bac4ba3ca0ac57f6e019
MaccS <maksmoskalenko2008@gmail.com>
2023-10-21 17:30:22 +0300

    add new file

--- Cat project 2.0.py
+++ Cat project 2.0.py
@@ -3,13 +3,13 @@ class Cat:
         print("start creating cat")
         self.name = name
         self.hp = 100
-        self.score = 0
-        print("start creating character cat")
+        self.sleep = 0
+        print("finish creating the cat")
 
     def show_info(self):
         print("Name:", self.name)
         print("Hp:", self.hp)
-        print("Score:", self.sleep)
+        print("Sleep energy:", self.sleep)
 
     def set_name(self, new_name):
         self.name = new_name
@@ -20,26 +20,32 @@ class Cat:
         else:
             print("error hp")
 
-    def set_score(self, sleep):
+    def set_sleep(self, sleep):
         if sleep > 0:
-            self.score = sleep
+            self.sleep - sleep
 
     def is_alive(self):
         return self.hp > 0
 
     def damage(self, amount):
-        self.hp -= amount
+        self.sleep-=amount
+        self.hp-=self.sleep
+        if self.hp<0:
+            print("Cat died")
 
-    def add_score(self, amount):
+
+    def add_sleep(self, amount):
         self.sleep += amount
 
     def heal(self, hp):
-        self.hp += hp
+        if self.sleep > 15:
+            self.hp += hp
+
 
 
 from random import randint
 
-cat = Cat("")
+cat = Cat("Dog")
 cat.show_info()
 
 while cat.is_alive():
